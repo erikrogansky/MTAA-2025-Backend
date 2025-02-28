41e5491887a38a73cee42c03fc2e988b1de2d7a1
erikrogansky <roganskyerik@gmail.com>
2025-02-28 09:15:47 +0100

    fix: migration fix

--- prisma/migrations/20250228081103_user/migration.sql
+++ prisma/migrations/20250228081103_user/migration.sql
@@ -3,6 +3,8 @@ CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "preferences" TEXT[],
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
