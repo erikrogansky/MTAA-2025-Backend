f821c9c0c6c7babed9b75bcf53ada82a17eef5fa
erikrogansky <roganskyerik@gmail.com>
2025-03-08 10:16:52 +0100

    fix: type in socket

--- socket-manager.js
+++ socket-manager.js
@@ -17,23 +17,15 @@ function handleMessage(userId, message, ws) {
 }
 
 function sendMessageToUser(userId, message) {
-    const { userSockets } = require("./socket"); // Lazy import
+    const { userSockets } = require("./socket");
     if (userSockets.has(userId)) {
         userSockets.get(userId).forEach((ws) => {
+            if (!message.type) {
+                message.type = "unknown";
+            }
             ws.send(JSON.stringify(message));
         });
     }
 }
 
-function removeUserSessions(userId) {
-    const { userSockets } = require("./socket"); // Lazy import
-    if (userSockets.has(userId)) {
-        userSockets.get(userId).forEach((ws) => {
-            ws.send(JSON.stringify({ type: "force_logout", message: "You have been logged out." }));
-            ws.close();
-        });
-        userSockets.delete(userId);
-    }
-}
-
-module.exports = { handleMessage, sendMessageToUser, removeUserSessions };
+module.exports = { handleMessage, sendMessageToUser };
