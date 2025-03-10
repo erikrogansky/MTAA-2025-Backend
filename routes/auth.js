21544529c8547541d86dccdc05c2d22bc99eca38
erikrogansky <roganskyerik@gmail.com>
2025-03-10 20:17:28 +0100

    fix: route definition

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 
 const express = require('express');
-const { register, login, refreshAccessToken, logout, logoutAll } = require('../controllers/authController');
+const { register, login, oauthLogin, refreshAccessToken, logout, logoutAll } = require('../controllers/authController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/oauth", oauthLogin);
 router.post("/refresh-token", refreshAccessToken);
 router.post("/logout", authMiddleware, logout);
 router.post("/logout-all", authMiddleware, logoutAll);
