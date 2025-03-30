d338126db33ae1bac14560562c012ecb3726d87a
erikrogansky <roganskyerik@gmail.com>
2025-03-30 12:39:30 +0200

    feat: added post review route

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { uploadRecipeImages, logFields } = require('../utils/multerRecipe');
-const { createRecipe, getAllOwnRecipes, getRecipeById } = require('../controllers/recipeController');
+const { createRecipe, getAllOwnRecipes, getRecipeById, addReview } = require('../controllers/recipeController');
 
 const router = express.Router();
 
 router.post('/create', logFields, uploadRecipeImages, createRecipe);
 router.get('/get-own', getAllOwnRecipes)
 router.get('/get-by-id/:id', getRecipeById);
+router.post('/post-review', addReview);
 
 module.exports = router;
