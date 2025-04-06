b71b00d374547ed7a569fc2b4389f7317b18a046
erikrogansky <roganskyerik@gmail.com>
2025-04-06 08:46:19 +0200

    feat: change creating recipe to handle updating it

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,13 +1,9 @@
 const { format, formatDistanceToNow, differenceInDays } = require('date-fns');
 const { prisma } = require("../db");
-const { is } = require('date-fns/locale');
 
 const createRecipe = async (req, res) => {
     try {
-        const { title, tags, ingredients, instructions, isPublic, description, details } = req.body;
-
-        console.log('Incoming fields:', req.body);
-        console.log('Incoming files:', req.files);
+        const { recipeId, title, tags, ingredients, instructions, isPublic, description, details } = req.body;
 
         const isPublicBoolean = isPublic === 'true'; 
 
@@ -15,7 +11,6 @@ const createRecipe = async (req, res) => {
         const imagePaths = req.files['images[]'] ? req.files['images[]'].map(file => file.filename) : [];
 
         const userId = req.user.id;
-
         const tagArray = tags.split(',');
 
         const tagRecords = await prisma.tag.findMany({
@@ -26,41 +21,85 @@ const createRecipe = async (req, res) => {
             }
         });
 
-        const recipe = await prisma.recipe.create({
-            data: {
-                title,
-                ingredients,
-                instructions,
-                isPublic: isPublicBoolean, 
-                coverPhoto,
-                description,
-                details,
-                userId,
-                tags: {
-                    connect: tagRecords.map(tag => ({ id: tag.id })) 
-                }
-            },
-        });
+        let recipe;
 
-        if (imagePaths.length > 0) {
-            await prisma.recipeImage.createMany({
-                data: imagePaths.map(image => ({
-                    recipeId: recipe.id,
-                    imagePath: image,
-                })),
+        if (recipeId) {
+            await prisma.recipe.update({
+                where: { id: recipeId },
+                data: {
+                    tags: { set: [] }
+                }
+            });
+
+            await prisma.recipeImage.deleteMany({
+                where: { recipeId }
+            });
+
+            recipe = await prisma.recipe.update({
+                where: { id: recipeId },
+                data: {
+                    title,
+                    ingredients,
+                    instructions,
+                    isPublic: isPublicBoolean,
+                    coverPhoto,
+                    description,
+                    details,
+                    tags: {
+                        connect: tagRecords.map(tag => ({ id: tag.id }))
+                    }
+                }
+            });
+
+            if (imagePaths.length > 0) {
+                await prisma.recipeImage.createMany({
+                    data: imagePaths.map(image => ({
+                        recipeId: recipe.id,
+                        imagePath: image,
+                    })),
+                });
+            }
+
+            res.status(200).json({
+                message: 'Recipe updated successfully',
+                recipe
+            });
+        } else {
+            recipe = await prisma.recipe.create({
+                data: {
+                    title,
+                    ingredients,
+                    instructions,
+                    isPublic: isPublicBoolean, 
+                    coverPhoto,
+                    description,
+                    details,
+                    userId,
+                    tags: {
+                        connect: tagRecords.map(tag => ({ id: tag.id })) 
+                    }
+                },
+            });
+
+            if (imagePaths.length > 0) {
+                await prisma.recipeImage.createMany({
+                    data: imagePaths.map(image => ({
+                        recipeId: recipe.id,
+                        imagePath: image,
+                    })),
+                });
+            }
+
+            res.status(201).json({
+                message: 'Recipe created successfully',
+                recipe
             });
         }
-
-        res.status(201).json({
-            message: 'Recipe created successfully',
-            recipe: recipe,
-        });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'An error occurred while creating the recipe', error: error.message });
+        res.status(500).json({ message: 'An error occurred while saving the recipe', error: error.message });
     }
 };
-
 
 const getAllOwnRecipes = async (req, res) => {
     try {
